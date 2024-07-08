876bb19a6e87ac213b2317b177649c9932fddda0
drmdg <diegobitencort@gmail.com>
2024-07-08 03:44:08 -0300

    initial commit

--- app/database/Connection.php
+++ app/database/Connection.php
@@ -15,7 +15,7 @@ class Connection{
         }
 
         try{
-            static::$pdo = new PDO('mysql:host=localhost;port=3306;dbname=blog1', 'root', '', [
+            static::$pdo = new PDO('mysql:host=localhost;port=3306;dbname=clinivet', 'root', '', [
                 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
