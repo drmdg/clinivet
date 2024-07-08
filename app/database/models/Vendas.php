fd9f4913bf62e659f410fd9de2d24bde4e8a3615
drmdg <diegobitencort@gmail.com>
2024-07-08 18:14:15 -0300

    Fazendo pag Home

--- app/database/models/Vendas.php
+++ app/database/models/Vendas.php
@@ -11,7 +11,7 @@ class Vendas extends Base{
     public function find($fetchAll = true){
 
         try{
-            $query = $this->connection->query('SELECT a.*, b.nome AS nome_cliente FROM `vendas` a, `user` b WHERE a.id_cliente = b.id');
+            $query = $this->connection->query('SELECT a.*, b.nome AS nome_cliente FROM `vendas` a, `user` b WHERE a.id_cliente = b.id ORDER BY a.data_venda DESC');
             return $fetchAll ? $query->fetchAll() : $query->fetch();
         }catch(PDOException $e){
             var_dump($e->getMessage());
